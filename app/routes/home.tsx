d83d331b5100578ed08e96b225165b87b576781c
AlexThukynov1 <alexthukynov@gmail.com>
2025-10-02 21:47:20 +0300

    add navbar

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,7 +9,13 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main>
-    
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar/>
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Ypur Applications and Resume Ratings</h1>
+        <h2>Review your submissions and check AI-powered feedback.</h2>
+      </div>
+    </section>
   </main>;
 }
