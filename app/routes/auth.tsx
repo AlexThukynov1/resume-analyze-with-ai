9a66fef1eec4ec8127e1afdd179ec7a6b6a8b57a
AlexThukynov1 <alexthukynov@gmail.com>
2025-10-08 14:43:56 +0300

    add auth rout and start page

---
+++ app/routes/auth.tsx
@@ -0,0 +1,14 @@
+export const  meta= () => ([
+    {title: "ResAI | Auth"},
+    {name: 'description', content: 'Description'}
+])
+
+const Auth = () => {
+    return (
+        <div>
+            Auth
+        </div>
+    );
+}
+
+export default Auth;
