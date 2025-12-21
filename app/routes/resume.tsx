ddbee08d46860bada3577cafdcf010b2d7cfab77
AlexThukynov1 <alexthukynov@gmail.com>
2025-12-21 23:25:41 +0200

    end section resume

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,6 +1,6 @@
-import {Link, useParams} from 'react-router';
+import {Link, useNavigate, useParams} from 'react-router';
 import type { Route } from "./+types/home";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { usePuterStore } from '~/lib/puter';
 
 export function meta({}: Route.MetaArgs) {
@@ -13,6 +13,10 @@ export function meta({}: Route.MetaArgs) {
 export default function resume() {
     const {auth, isLoading, fs, kv} = usePuterStore();
     const {id} = useParams();
+    const [resumeUrl, setResumeUrl] = useState('');
+    const [imageUrl, setImageUrl] = useState('');
+    const [feedback, setFeedback] = useState('');
+    const navigate = useNavigate();
 
     useEffect(() => {
         const loadResume = async () => {
@@ -23,7 +27,22 @@ export default function resume() {
 
             const resumeBlob = await fs.read(data.resumePath);
             if(!resumeBlob) return;
+
+            const pdfBlob = new Blob ([resumeBlob], {type: 'application/pdf'});
+            const resumeUrl = URL.createObjectURL(pdfBlob);
+
+            setResumeUrl(resumeUrl);
+
+            const imageBlob = await fs.read(data.imagePath);
+            if(!imageBlob) return ;
+            const imageUrl = URL.createObjectURL(imageBlob);
+            setImageUrl(imageUrl);
+
+            setFeedback(data.feedback);
+
         }
+
+        loadResume();
     }, [id])
 
   return (
@@ -38,9 +57,17 @@ export default function resume() {
             </Link>
         </nav>
         <div className='flex flex-row w-full max-lg:flex-col-reverse'>
-            <section className='feedback-section'>
+            <section className="feedback-section bg-[url('/images/bg-small.svg')] bg-cover h-[100vh] sticky top-0 items-center justify-center">
                 {imageUrl && resumeUrl && (
-                    <div className='animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-w:xl h-fit w-fit'></div>
+                    <div className='animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-w:xl h-fit w-fit'>
+                        <a href={resumeUrl} target="_blank" rel="noopener noreferrer">
+                            <img
+                                src={imageUrl}
+                                className='w-full h-full object-contain rounded-2xl'
+                                title="resume"
+                            />
+                        </a>
+                    </div>
                 )}
             </section>
         </div>
